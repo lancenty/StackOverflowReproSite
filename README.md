7a09863b1aaed61373b899f0d935067723e10c71
Lancent Yuan <lancent.yuan@gmail.com>
2019-03-17 22:58:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lab of a high CPU App Service site written in .Net Core
+[![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
---
+++ StackOverflow/Controllers/ReproController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StackOverflow.Controllers
+{
+    public class ReproController : Controller
+    {
+        // GET: Repro
+        public ActionResult StackOverflow()
+        {
+            ReproStackOverflow.stackOverflow();
+            return View();
+        }
+    }
+
+    public class ReproStackOverflow
+    {
+        public static Boolean stackOverflow()
+        {
+            stackOverflow();
+            return true;
+        }
+    }
+}
